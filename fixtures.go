0719e63fe3e300e732f3589e474fa3bfdc70b710
goliatone <140582+goliatone@users.noreply.github.com>
2024-11-28 10:14:52 -0800

    add: fixtures handler

---
+++ fixtures.go
@@ -0,0 +1,137 @@
+package persistence
+
+import (
+	"context"
+	"io/fs"
+	"strings"
+
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dbfixture"
+)
+
+// Fixtures manages fixtures and seeds
+type Fixtures struct {
+	dir        fs.FS
+	db         *bun.DB
+	truncate   bool
+	drop       bool
+	funcMap    TplMap
+	fixture    *dbfixture.Fixture
+	opts       []FixtureOption
+	FileFilter func(path, name string) bool
+}
+
+// FixtureOption configures the seed manager
+type FixtureOption func(s *Fixtures)
+
+// WithFS will truncate tables
+func WithFS(dir fs.FS) FixtureOption {
+	return func(s *Fixtures) {
+		s.dir = dir
+	}
+}
+
+// WithTrucateTables will truncate tables
+func WithTrucateTables() FixtureOption {
+	return func(s *Fixtures) {
+		s.truncate = true
+	}
+}
+
+// WithDropTables will drop tables
+func WithDropTables() FixtureOption {
+	return func(l *Fixtures) {
+		l.drop = true
+	}
+}
+
+// TplMap hosts function maps
+type TplMap map[string]any
+
+// WithTemplateFuncs are used to solve functions in seed file
+func WithTemplateFuncs(funcMap TplMap) FixtureOption {
+	return func(s *Fixtures) {
+		for k, v := range funcMap {
+			s.funcMap[k] = v
+		}
+	}
+}
+
+// WithFileFilter will add a file filter function.
+// Each file found in the given dir will be passed throu
+// this function, and if it returns false the file will
+// be filtered out.
+func WithFileFilter(fn func(path, name string) bool) FixtureOption {
+	return func(s *Fixtures) {
+		s.FileFilter = fn
+	}
+}
+
+// NewSeeManager generates a new seed manger
+func NewSeeManager(db *bun.DB, opts ...FixtureOption) *Fixtures {
+	s := &Fixtures{
+		db:      db,
+		opts:    opts,
+		funcMap: make(TplMap),
+		FileFilter: func(path, name string) bool {
+			return strings.HasSuffix(path, ".yml") || strings.HasSuffix(path, ".yaml")
+		},
+	}
+
+	return s
+}
+
+func (s *Fixtures) init() {
+	for _, o := range s.opts {
+		o(s)
+	}
+
+	opts := []dbfixture.FixtureOption{}
+	if s.drop {
+		opts = append(opts, dbfixture.WithRecreateTables())
+	} else if s.truncate {
+		opts = append(opts, dbfixture.WithTruncateTables())
+	}
+
+	// Recreate will drop existing table
+	s.fixture = dbfixture.New(s.db, opts...)
+}
+
+// AddOptions will configure options
+func (s *Fixtures) AddOptions(opts ...FixtureOption) *Fixtures {
+	s.opts = append(s.opts, opts...)
+	return s
+}
+
+// Load will load a file
+func (s *Fixtures) Load(ctx context.Context) error {
+
+	if s.fixture == nil {
+		s.init()
+	}
+
+	return fs.WalkDir(s.dir, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		if !s.FileFilter(path, d.Name()) {
+			return nil
+		}
+
+		if err := s.LoadFile(ctx, path); err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
+// LoadFile will load a file
+func (s *Fixtures) LoadFile(ctx context.Context, file string) error {
+	return s.fixture.Load(ctx, s.dir, file)
+}
